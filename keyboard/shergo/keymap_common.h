a0c23f510a1f0b4f2f9890a62128d3a7ad695576
shaymdev <shaymdev@gmail.com>
2015-08-11 15:33:11 -0600

    Switched columns 3 and 4 and fixed tab/enter
    Switched columns 3 and 4 because my wiring was wrong and fixed the tab and enter keys that were wrong.

--- keyboard/shergo/keymap_common.h
+++ keyboard/shergo/keymap_common.h
@@ -35,6 +35,7 @@ extern const uint16_t fn_actions[];
 
 
 /* shergo keymap definition macro
+ * note i switched colum 3 and 4 because i wired it wrong
  */
 #define KEYMAP( \
     K00, K01, K02, K03, K04, K05, K06, K07, \
@@ -43,11 +44,11 @@ extern const uint16_t fn_actions[];
     K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, \
     K40, K41, K42, K43, K44, K45, K46, K47, K48, K49 \
 ) { \
-    { KC_##K01, KC_##K02, KC_##K02, KC_##K03, KC_##K04, KC_##K05, KC_##K06, KC_##K07, KC_NO, KC_NO}, \
-    { KC_##K10, KC_##K11, KC_##K12, KC_##K13, KC_##K14, KC_##K15, KC_##K16, KC_##K17, KC_##K18, KC_##K19 }, \
-    { KC_##K20, KC_##K21, KC_##K22, KC_##K23, KC_##K24, KC_##K25, KC_##K26, KC_##K27, KC_##K28, KC_##K29 }, \
-    { KC_##K30, KC_##K31, KC_##K32, KC_##K33, KC_##K34, KC_##K35, KC_##K36, KC_##K37, KC_##K38, KC_##K39 }, \
-    { KC_##K40, KC_##K41, KC_##K42, KC_##K43, KC_##K44, KC_##K45, KC_##K46, KC_##K47, KC_##K48, KC_##K49 }  \
+    { KC_##K00, KC_##K01, KC_##K02, KC_##K04, KC_##K03, KC_##K05, KC_##K06, KC_##K07, KC_NO, KC_NO}, \
+    { KC_##K10, KC_##K11, KC_##K12, KC_##K14, KC_##K13, KC_##K15, KC_##K16, KC_##K17, KC_##K18, KC_##K19 }, \
+    { KC_##K20, KC_##K21, KC_##K22, KC_##K24, KC_##K23, KC_##K25, KC_##K26, KC_##K27, KC_##K28, KC_##K29 }, \
+    { KC_##K30, KC_##K31, KC_##K32, KC_##K34, KC_##K33, KC_##K35, KC_##K36, KC_##K37, KC_##K38, KC_##K39 }, \
+    { KC_##K40, KC_##K41, KC_##K42, KC_##K44, KC_##K43, KC_##K45, KC_##K46, KC_##K47, KC_##K48, KC_##K49 }  \
 }
 
 #endif
